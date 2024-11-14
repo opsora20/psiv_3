34a214b3bd6dc9528486c1ff4d5d04a3b45015f5
joanber024 <joansenior918273@gmail.com>
2024-11-14 14:29:21 +0100

    Train

--- modules/train.py
+++ modules/train.py
@@ -3,9 +3,15 @@ import copy
 import torch
 import matplotlib.pyplot as plt
 import wandb
+import sys
 
 
 def train_autoencoder(model, batch_size, loss_func, device, loader, optimizer, num_epochs):
+    
+    loss_log = {x:[] for x in list(loader.keys())}
+    
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss = sys.maxsize
     
     for epoch in range(1, num_epochs+1):
         print('Epoch {}/{}'.format(epoch, num_epochs))
@@ -13,25 +19,52 @@ def train_autoencoder(model, batch_size, loss_func, device, loader, optimizer, n
         
         t0 = time.time()
         
-        train_epoch(model, batch_size, )
+        train_epoch(model, batch_size, loss_func, device, loader, optimizer, loss_log)
+        
+        epoch_time = time.time() - t0
+        
+        if loss_log['train'][-1] < best_loss:
+            best_loss = loss_log['train'][-1]
+            best_epoch = epoch
+        
+        print("Epoch elapsed time: {:.4f}s \n".format(epoch_time))
         
         
+    print('Best val Loss: {:4f} at epoch {}'.format(best_loss, best_epoch))
+    model.load_state_dict(best_model_wts)
         
 
 
 
-def train_epoch(model, batch_size, loss_func, device, loader, optimizer):
-    
-    model.train()
-    
-    running_loss = 0.0
-    batch_acc = 0.0
-    
-    for batch_idx, inputs in enumerate(loader):
-        inputs = inputs.to(device)
+def train_epoch(model, batch_size, loss_func, device, loader, optimizer, loss_log):
+    for phase in list(loader.keys()):
+        if phase == 'train':
+            model.train()  
+        else:
+            model.eval()  
+
+        running_loss = 0.0
+        batch_acc = 0.0
         
-        optimizer.zero_grad()
+        for batch_idx, inputs in enumerate(loader[phase]):
+            inputs = inputs.to(device)
+            
+            optimizer.zero_grad()
+            
+            outputs = model(inputs)
+            
+            loss = loss_func(inputs, outputs)
+            
+            if phase == 'train':
+                loss.backward()
+                optimizer.step()
+                
+                
+            running_loss += loss
+            
+        epoch_loss = running_loss/len(loader[phase])
         
-        outputs = model(inputs)
-        loss.backward()
-        optimizer.step()
+        print('{} Loss:{:.4f}'.format(phase, epoch_loss))
+        loss_log[phase].append(epoch_loss)
+        
+        return model, loss_log
