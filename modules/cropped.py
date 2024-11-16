c50e931ff0d850f464d3ed9a20bcfd22b7c2bdf7
sergio <sergio@MacBook-Pro-de-sergio.local>
2024-11-16 23:13:50 +0100

    cropped load error

--- modules/cropped.py
+++ modules/cropped.py
@@ -3,6 +3,7 @@ import pandas as pd
 from skimage import io, color
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
+from utils import echo
 
 
 def load_cropped_patients(cropped_dir, cropped_csv):
@@ -18,13 +19,16 @@ def load_cropped_patients(cropped_dir, cropped_csv):
                 if(file_img.endswith(".png")):
                     image = io.imread(os.path.join(cropped_dir, patient_dir, file_img))
                     image = color.rgba2rgb(image)
+                    echo(image.shape)
                     if (image.shape[0] != 256 or image.shape[1] != 256):
-                        print(file_img, image.shape)
+                        echo(file_img, image.shape)
                     else:
                         image = image.transpose(2, 0, 1)
                         imgs.append(image)
         
     return np.array(imgs)
+
+
 
 
 def create_dataloaders(class_dataset, batch):
