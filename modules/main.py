2ba52a6869271c796b0cd5f9f0f72d5461fd5422
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2024-11-16 19:59:37 +0100

    Echo fix 4

--- modules/main.py
+++ modules/main.py
@@ -21,6 +21,8 @@ import gc
 from train import train_autoencoder
 from losses import MSE_loss
 
+from utils import echo
+
 
 ROOT_DIR = "../HelicoDataSet/CrossValidation/Cropped"
 csv_filename = "../HelicoDataSet/PatientDiagnosis.csv"
@@ -70,20 +72,20 @@ def AEConfigs(Config):
 
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    os.system(f"echo '{device}'")
+    echo(device)
     Config = '1'
     net_paramsEnc, net_paramsDec, inputmodule_paramsDec, inputmodule_paramsEnc = AEConfigs(
         Config)
     model = AutoEncoderCNN(inputmodule_paramsEnc, net_paramsEnc,
                            inputmodule_paramsDec, net_paramsDec)
-    os.system(f"echo '{model}'")
+    echo(model)
     model.to(device)
 
-    os.system("echo 'Reading Dataset...'")
+    echo('Reading Dataset...')
     data = HelicoDataset(csv_filename, ROOT_DIR, read_images=True)
     batch_size = 16
     dataloader = create_dataloaders(data, batch_size)
-    os.system("echo 'Dataset Readed'")
+    echo('Dataset Readed')
     # 0. EXPERIMENT PARAMETERS
     # 0.1 AE PARAMETERS
 
---
+++ modules/utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*- noqa
+"""
+Created on Sat Nov 16 19:55:15 2024
+
+@author: Joel tapia Salvador
+"""
+import os
+
+
+def echo(out):
+    os.system(f"echo '{out}'")
