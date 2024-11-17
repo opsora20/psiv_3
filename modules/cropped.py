b645a0c7baad27f0b73886e25decb3303333b395
Almoujtaba <91468363+Almoujtaba@users.noreply.github.com>
2024-11-17 16:19:53 +0100

    Update cropped.py

--- modules/cropped.py
+++ modules/cropped.py
@@ -32,7 +32,30 @@ def load_cropped_patients(cropped_dir, cropped_csv):
 
 
 def load_annotated_patients(annotated_dir, annotated_excel):
-    pass
+    patches = []
+    labels_list = []
+    df_annotations = pd.read_excel(annotated_excel)
+    for idx, row in df_annotations.iterrows():
+        file = row['FILENAME']
+        label = row['LABEL']
+        file_path = os.path.join(annotated_dir, file)
+        if os.path.isfile(file_path):
+            im = io.imread(file_path)
+            if im.ndim == 2:
+                im = np.stack((im,)*3, axis=-1)
+            elif im.shape[2] > 3:
+                im = im[:, :, :3]
+            if im.shape[0] != 256 or im.shape[1] != 256:
+                echo(f"Tamaño incorrecto para {file}: {im.shape}")
+                continue
+            else:
+                im = im.transpose(2, 0, 1)
+                patches.append(im)
+                labels_list.append(label)
+        else:
+            echo(f"Archivo no encontrado: {file_path}")
+
+    return np.array(patches), np.array(labels_list)
 
 
 def create_dataloaders(class_dataset, batch):
